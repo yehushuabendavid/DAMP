5de090dbb844ffe2cbe848cf1b7ccef083101ef9
yosh <yehushua.ben.david@gmail.com>
2018-09-25 11:19:32 +0300

    DoneOne

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,21 @@
 FROM ubuntu
-RUN apt update; \
-apt-get -y  install dialog vim nano net-tools   
+ARG PASSWORD=root
+RUN apt update
+RUN apt-get -y  install apt-utils vim nano net-tools  debconf-utils 
+RUN export DEBIAN_FRONTEND=noninteractive ; apt-get -y install mysql-server ;
+RUN /etc/init.d/mysql start ; mysql -uroot -e"UPDATE mysql.user SET plugin = 'mysql_native_password', authentication_string = PASSWORD('$PASSWORD') WHERE user = 'root'"
+RUN export DEBIAN_FRONTEND=noninteractive ; apt-get -y install  php apache2 phpmyadmin
+RUN echo "phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2" | debconf-set-selections ;\
+dpkg-reconfigure -f nointeractive phpmyadmin
+RUN apt-get -y install memcached ssh ;\
+echo "PermitRootLogin YES" >> /etc/ssh/sshd_config ;\
+echo "root:$PASSWORD" | chpasswd
+RUN echo "#!/bin/bash" > startAll.sh ;\
+echo "/etc/init.d/mysql restart" >> startAll.sh;\
+echo "/etc/init.d/memcached restart" >> startAll.sh;\
+echo "/etc/init.d/apache2 restart" >> startAll.sh;\
+echo "/etc/init.d/ssh restart" >> startAll.sh;\
+echo "bash" >> startAll.sh;\
+chmod +x startAll.sh
+
 
--- build.sh
+++ build.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
-if [ "$( id -u)" != "0" ]
-	then
-		echo "Need to ROOT"
-		exit
-fi
-
-if [ "$( which docker)" == "" ] 
-	then
-		echo "Docker not found"
-		exit
-fi 
-
+if [ "$( id -u)" != "0" ]; then	echo "Need to ROOT"; exit; fi
+if [ "$( which docker)" == "" ]; then echo "Docker not found"; exit; fi
 echo "--------------BUILD DockerApache2MysqlPHP -------------"
-docker build -t damp .
+echo -n "Enter the password for root user (system & mysql) [rootpw] ?"
+read RPW
+if [ "Y$RPW" == "Y" ];       then RPW="rootpw" ; fi
+docker build --build-arg PASSWORD=$RPW -t damp .
+echo -n "Run it [Y] ?"
+read DORUNIT
+if [ "Y$DORUNIT" == "Y" ];	 then 	DORUNIT="Y" ; fi
+if [ "${DORUNIT^^}" == "YES" ] ; then 	DORUNIT="Y" ; fi
+if [ "$DORUNIT" != "Y" ] ;	 then 	echo "You can run it like this:";echo "docker run -it -p 8080:80 -p 2222:22 damp bash startAll.sh";exit;fi
+echo -n "Select the localhost port for HTTP [8080] ?"
+read PHTTP
+echo -n "Select the localhost port for SSH [2222] ?"
+read PSSH
+
+if [ "$PHTTP" == "" ] ; then PHTTP=8080 ; fi
+if [ "$PSSH" == "" ] ; then PSSH=2222 ; fi
+
+docker run -it -p $PHTTP:80 -p $PSSH:22 --rm --name damptest damp /startAll.sh
+
+
+
+
 
